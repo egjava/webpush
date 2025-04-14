48033935e877292175c903c3bb5a097416d081f6
Elizabeth Gladys Cherupunathil Mani <egjava@yahoo.com>
2025-04-14 15:51:33 -0700

    added few changes

--- client/client.js
+++ client/client.js
@@ -23,7 +23,7 @@ async function send() {
     alert('[Service Worker] Registered. Scope::', register.scope);
     console.log("Service Worker Registered...");
     //const registration = await navigator.serviceWorker.ready; // Here's the waiting
-    await navigator.serviceWorker.ready;
+    const registration = await navigator.serviceWorker.ready;
     alert('register ready')
     if (!register.pushManager) {
         alert("not registered pushmanager")
@@ -34,7 +34,7 @@ async function send() {
     // Register Push
     console.log("Registering Push...");
     alert("Registering Push...");
-    const subscription = await register.pushManager.subscribe({
+    const subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
         applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
     }).catch((err) => { return alert('Web Push] Registration Error:', err) });
