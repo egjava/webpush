73f84012af22388f7e2aa0d178bdccbba5b76987
Elizabeth Gladys Cherupunathil Mani <egjava@yahoo.com>
2025-04-14 16:26:12 -0700

    added permission check

--- client/client.js
+++ client/client.js
@@ -1,9 +1,96 @@
 const publicVapidKey =
     "BKCOr589xStA1gSVk_RDKlbtgnqSYxDWLcy6CJVFq7Bep9sqzS5rwM75RIZIiSBbhfkEtm38R7RFrjyk6-9F4yY";
-   
+
 alert('here');
+const main = async () => {
+    alert("inside main")
+    check();
+    const permission = await requestNotificationPermission();
+    const swRegistration = await registerServiceWorker();
+};
+const check = () => {
+    alert("inside check")
+    if (!("serviceWorker" in navigator)) {
+        alert("No Service Worker support!");
+        throw new Error("No Service Worker support!");
+    }
+    else {
+        alert("serviceworker ready");
+        console.log("serviceworker:", navigator.serviceWorker);
+    }
+     if (!("PushManager" in window)) {
+         alert("No Push API Support!");
+         if (!isPushManagerActive(pushManager)) {
+             alert("not active return");
+             return;
+         }
+         throw new Error("No Push API Support!");
+     }
+     else {
+         alert("PushManager ready");
+     }
+};
+
+function isPushManagerActive(pushManager) {
+    alert("isPushmanager");
+    if (!pushManager) {
+        if (!window.navigator.standalone) {
+            alert("inside standalone");
+            //document.getElementById('add-to-home-screen').style.display = 'block';
+        } else {
+            alert("PushManager not active");
+            throw new Error('PushManager is not active');
+        }
+        alert("before subscribebtn");
+        // document.getElementById('subscribe_btn').style.display = 'none';
+        return false;
+    } else {
+        return true;
+    }
+}
+
+
+const registerServiceWorker = async () => {
+    alert("inside registerservicworker");
+    const swRegistration = await navigator.serviceWorker.register("/worker.js");
+    console.log('[Service Worker] Registered. Scope::', swRegistration.scope);
+    alert('[Service Worker] Registered. Scope::', swRegistration.scope);
+    console.log("Service Worker Registered...");
+    //const registration = await navigator.serviceWorker.ready; // Here's the waiting
+    const register = await navigator.serviceWorker.ready;
+    alert('register ready')
+    alert(register);
+    console.log("registereeeeeeeeee", register)
+    let pushManager = register.pushManager;
+    if (!isPushManagerActive(pushManager)) {
+        alert("no pushmanager");
+        return;
+    }
+    /*if (!swRegistration.pushManager) {
+        alert("not registered pushmanager")
+        throw { errorCode: "PushManagerUnavailable" };
+    } else {
+        alert("registered pushmanager")
+    }*/
+    // Register Push
+    console.log("Registering Push...");
+    alert("Registering Push...");
+    const subscription = await pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+    }).catch((err) => { return console.log('Web Push] Registration Error:', err) });
+    console.log("Push Registered...");
+    alert("Push Registered...");
+    alert("Sending Push...");
+    // Send Push Notification
+    const response = await saveSubscription(subscription);
+    console.log(response);
+    console.log("Push Sent...");
+    alert("Push Sent...");
+    // return swRegistration;
+};
 // Check for service worker
-if ("serviceWorker" in navigator) { 
+/*if ("serviceWorker" in navigator) { 
     alert('calling send');
     send().catch(err => console.error(err));
 }
@@ -23,7 +110,7 @@ async function send() {
     alert('[Service Worker] Registered. Scope::', register.scope);
     console.log("Service Worker Registered...");
     //const registration = await navigator.serviceWorker.ready; // Here's the waiting
-    const registration = await navigator.serviceWorker.ready;
+    await navigator.serviceWorker.ready;
     alert('register ready')
     if (!register.pushManager) {
         alert("not registered pushmanager")
@@ -34,7 +121,7 @@ async function send() {
     // Register Push
     console.log("Registering Push...");
     alert("Registering Push...");
-    const subscription = await registration.pushManager.subscribe({
+    const subscription = await register.pushManager.subscribe({
         userVisibleOnly: true,
         applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
     }).catch((err) => { return alert('Web Push] Registration Error:', err) });
@@ -52,8 +139,73 @@ async function send() {
     });
     console.log("Push Sent...");
    alert("Push Sent...");
-}
+}*/
+const saveSubscription = async subscription => {
+    alert('calling save subscription');
+    //const SERVER_URL = `${hostName}/subscribe`;
+    const response = await fetch("/subscribe", {
+        method: "POST",
+        body: JSON.stringify(subscription),
+        headers: {
+            "content-type": "application/json"
+        }
+    });
+    /* const response = await fetch(SERVER_URL, {
+         method: "post",
+         headers: {
+             "Content-Type": "application/json"
+         },
+         body: JSON.stringify(subscription)
+     });*/
+    return response.json();
+};
 
+
+const requestNotificationPermission = async () => {
+    alert("inside requestNotificationPermission");
+    const permission = await window.Notification.requestPermission();
+    console.log("permission", permission);
+    switch (permission) {
+        case 'prompt':
+            alert("prompt");
+            break;
+        case 'granted':
+            alert("granted");
+            break;
+        case 'denied':
+            alert('User denied push permission');
+        case 'default':
+            alert('default');
+    }
+    // value of permission can be 'granted', 'default', 'denied'
+    // granted: user has accepted the request
+    // default: user has dismissed the notification permission popup by clicking on x
+    // denied: user has denied the request.
+
+    Notification.requestPermission().then(function (permission) {
+        console.log('permiss', permission)
+    });
+    if (permission !== "granted") {
+        alert("permission not granted");
+        //throw new Error("Permission not granted for Notification");
+    }
+};
+
+const send = async () => {
+    alert("inside send")
+    // const SERVER_URL = `${hostName}/send-notification`;
+    // console.log("Server_url send****", SERVER_URL)
+    const response = await fetch("/send-notification", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+    });
+
+    return response.json();
+
+};
 function urlBase64ToUint8Array(base64String) {
     alert("urlBase64ToUint8Array");
     const padding = "=".repeat((4 - base64String.length % 4) % 4);
--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require("express");
 const webpush = require("web-push");
 const bodyParser = require("body-parser");
 const path = require("path");
-
+const cors = require('cors')
 const app = express();
 
 // Set static path
 app.use(express.static(path.join(__dirname, "client")));
-
+app.use(cors())
 app.use(bodyParser.json());
-
+const dummyDb = { subscription: null } //dummy in memory store
 const publicVapidKey =
     "BKCOr589xStA1gSVk_RDKlbtgnqSYxDWLcy6CJVFq7Bep9sqzS5rwM75RIZIiSBbhfkEtm38R7RFrjyk6-9F4yY";
 const privateVapidKey = "O3UYIHzRdSvCooG3VaQrbnoW-V52U9lnR-FItS8_zZY";
@@ -20,12 +20,19 @@ webpush.setVapidDetails(
     privateVapidKey
 );
 
+const saveToDatabase = async subscription => {
+    // Since this is a demo app, I am going to save this in a dummy in memory store. Do not do this in your apps.
+    // Here you should be writing your db logic to save it.
+    dummyDb.subscription = subscription
+}
 // Subscribe Route
 app.post("/subscribe", (req, res) => {
     // Get pushSubscription object
     const subscription = req.body;
-    console.log("Subscription*****",subscription)
+    console.log("Subscription*****", subscription)
+    saveToDatabase(subscription) //Method to save the subscription to Database
     // Send 201 - resource created
+    console.log("saved")
     res.status(201).json({});
 
     // Create payload
@@ -36,6 +43,18 @@ app.post("/subscribe", (req, res) => {
         .sendNotification(subscription, payload)
         .catch(err => console.error(err));
 });
+//function to send the notification to the subscribed device
+const sendNotification = (subscription, dataToSend) => {
+    webpush.sendNotification(subscription, dataToSend)
+}
+app.get('/send-notification', async (req, res) => {
+    console.log("send-notification")
+    const subscription = dummyDb.subscription //get subscription from your databse here.
+    console.log("subscription***", subscription);
+    const payload = JSON.stringify({ title: "Second Push Test by Elizabeth", message: "Elizabeth please check your email" });
+    sendNotification(subscription, payload)
+    res.json({ message: 'message sent' })
+})
 
 /*const port = 5000;
 
